02abb1cf8f86f8f7da038198809c9b7bbc7ba41b
Lucia Enriquez Bos <81951721+luciaenriquezbos@users.noreply.github.com>
2024-05-23 10:54:00 +0200

    styles page venue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   document.getElementById('menu-toggle').addEventListener('change', function() {
-//     if (this.checked) {
-//       document.body.style.overflow = 'hidden';
-//     } else {
-//       document.body.style.overflow = '';
-//     }
-//   });
-// });
-
-
-// document.getElementById('close-menu').addEventListener('click', function() {
-//     document.getElementById('menu-toggle').checked = false;
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
   var menuToggle = document.getElementById('menu-toggle');
   var btnExit = document.querySelector('.btn-exit');
@@ -20,18 +5,18 @@ document.addEventListener('DOMContentLoaded', function() {
   menuToggle.addEventListener('change', function() {
     if (this.checked) {
       document.body.style.overflow = 'hidden';
-      btnExit.style.display = 'block'; // Mostramos el botón de salir
+      btnExit.style.display = 'block'; 
     } else {
-      document.body.style.overflow = ''; // Restablecemos el overflow a su valor predeterminado
-      btnExit.style.display = 'none'; // Ocultamos el botón de salir
+      document.body.style.overflow = ''; 
+      btnExit.style.display = 'none'; 
     }
   });
 
-  // Agregamos un event listener al botón de salir para cerrar el menú
+  
   btnExit.addEventListener('click', function() {
     menuToggle.checked = false;
-    var event = new Event('change'); // Creamos un nuevo evento 'change'
-    menuToggle.dispatchEvent(event); // Desencadenamos el evento 'change'
+    var event = new Event('change'); 
+    menuToggle.dispatchEvent(event); 
   });
 });
 
