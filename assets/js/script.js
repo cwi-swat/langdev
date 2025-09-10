8974d1d17bf898c0179d6b30072b68e12d44b81b
Pedro J. Molina <pjmolina@metadev.pro>
2025-09-10 12:56:02 +0200

    fix to string (js error)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var menuToggle = document.getElementById('menu-toggle');
-  var btnExit = document.querySelector('.btn-exit');
+document.addEventListener("DOMContentLoaded", function () {
+  var menuToggle = document.getElementById("menu-toggle");
+  var btnExit = document.querySelector(".btn-exit");
 
-  menuToggle.addEventListener('change', function() {
+  menuToggle.addEventListener("change", function () {
     if (this.checked) {
-      document.body.style.overflow = 'hidden';
-      btnExit.style.display = 'block'; 
+      document.body.style.overflow = "hidden";
+      btnExit.style.display = "block";
     } else {
-      document.body.style.overflow = ''; 
-      btnExit.style.display = 'none'; 
+      document.body.style.overflow = "";
+      btnExit.style.display = "none";
     }
   });
 
-  
-  btnExit.addEventListener('click', function() {
+  btnExit?.addEventListener("click", function () {
     menuToggle.checked = false;
-    var event = new Event('change'); 
-    menuToggle.dispatchEvent(event); 
+    var event = new Event("change");
+    menuToggle.dispatchEvent(event);
   });
 });
-
-
-
-
-
-
-  
